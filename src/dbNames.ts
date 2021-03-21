571030ab4375b7d7aa10168922da821c157de827
ThayalanGR <grthayalan18@gmail.com>
2021-03-21 21:38:49 +0530

    basedb to coredb renaming

--- src/dbNames.ts
+++ src/dbNames.ts
@@ -1,4 +1,4 @@
 export const DB_NAMES = {
-    BASE_DB: 'BASE_DB',
+    CORE_DB: 'CORE_DB',
     // TENANT_DB: it will be provisoned dynamically and it will have tenant's id as name.
 };
--- src/models/index.ts
+++ src/models/index.ts
@@ -2,5 +2,5 @@
 export { default as MONGOOSE_MODELS } from './mongooseModels';
 
 // database models exports
-export * as baseDbModels from './baseDb';
+export * as coreDbModels from './coreDb';
 export * as tenantDbModels from './tenantDb';
