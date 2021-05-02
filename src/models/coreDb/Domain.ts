7a0efb1390767727f3f46f024126cb8ffd509569
ThayalanGR <grthayalan18@gmail.com>
2021-05-02 20:25:59 +0530

    domain service changes

--- src/models/coreDb/Domain.ts
+++ src/models/coreDb/Domain.ts
@@ -29,7 +29,7 @@ const DomainSchema = new Schema(
         },
         isReserved: {
             type: Schema.Types.Boolean,
-            default: true,
+            default: false,
         },
     },
     {
--- src/services/coreDb/domain.ts
+++ src/services/coreDb/domain.ts
@@ -4,14 +4,29 @@ import { DbConnectionManager } from '../../configs/initializer';
 import { MONGOOSE_MODELS } from '../../models';
 import { IDomain } from '../../models/coreDb/Domain';
 
-export const createDomain = async (domainName: string, tenantId: string): Promise<IDomain> => {
+export const createDomain = async ({
+    domainName,
+    tenantId,
+    isCustom = false,
+    isReserved = false,
+}: {
+    domainName: string;
+    tenantId: string;
+    isReserved?: boolean;
+    isCustom?: boolean;
+}): Promise<IDomain> => {
     const conn = DbConnectionManager.getCoreDb();
     const Domain = conn.model<IDomain>(MONGOOSE_MODELS.CORE_DB.DOMAIN);
     if (!checkDomainAvailability(domainName)) {
         logger.error(`Tenant invalid - domain already exist ${domainName}`);
         throw new BadRequestError(ERROR_CODE.TENANT_INVALID, 'Domain already exist');
     }
-    const domain = await Domain.create({ name: domainName, tenant: tenantId });
+    const domain = await Domain.create({
+        name: domainName,
+        tenant: tenantId,
+        isReserved,
+        isCustom,
+    });
     logger.info(`Tenant ${tenantId} has been mapped to ${domainName}.`);
     return domain;
 };
