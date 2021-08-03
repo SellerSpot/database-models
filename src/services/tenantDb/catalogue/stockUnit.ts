ce7062338a5e28ffbcd8de2b06659e526da1413f
Rohit Raj <rij7u7d@gmail.com>
2021-08-03 18:42:05 +0530

    Migrated StockUnit Service to new structure

--- src/services/tenantDb/catalogue/stockUnit.ts
+++ src/services/tenantDb/catalogue/stockUnit.ts
@@ -11,74 +11,71 @@ import { defaultStockUnits } from '../../../seeds/tenantDb/catalogue/StockUnit';
 import { isEmpty } from 'lodash';
 import { Model } from 'mongoose';
 
-export const createStockUnit = async (
-    newStockUnit: ICreateStockUnitRequest,
-): Promise<IStockUnitDoc> => {
-    const { name } = newStockUnit;
-    const StockUnit = DbConnectionManager.getTenantModel<IStockUnitDoc>(
-        MONGOOSE_MODELS.TENANT_DB.CATALOGUE.STOCKUNIT,
-    );
-    const isStockUnitExist = await StockUnit.exists({ name });
-    if (isStockUnitExist) {
-        throw new BadRequestError(
-            ERROR_CODE.STOCK_UNIT_NAME_INVALID,
-            'Stock unit name already exist',
+export class StockUnitDbService {
+    static getModal = (): Model<IStockUnitDoc> => {
+        return DbConnectionManager.getTenantModel<IStockUnitDoc>(
+            MONGOOSE_MODELS.TENANT_DB.CATALOGUE.STOCKUNIT,
         );
-    }
-    const stockUnit = await StockUnit.create(newStockUnit);
-    return stockUnit;
-};
+    };
 
-export const searchStockUnit = async (query: string): Promise<IStockUnitDoc[]> => {
-    const StockUnit = DbConnectionManager.getTenantModel<IStockUnitDoc>(
-        MONGOOSE_MODELS.TENANT_DB.CATALOGUE.STOCKUNIT,
-    );
-    const matchingStockUnits = await StockUnit.find({
-        $or: [{ name: new RegExp(`^${query}`, 'i') }, { unit: new RegExp(`^${query}`, 'i') }],
-    });
-    return matchingStockUnits;
-};
+    static createStockUnit = async (
+        newStockUnit: ICreateStockUnitRequest,
+    ): Promise<IStockUnitDoc> => {
+        const { name } = newStockUnit;
+        const StockUnit = StockUnitDbService.getModal();
+        const isStockUnitExist = await StockUnit.exists({ name });
+        if (isStockUnitExist) {
+            throw new BadRequestError(
+                ERROR_CODE.STOCK_UNIT_NAME_INVALID,
+                'Stock unit name already exist',
+            );
+        }
+        const stockUnit = await StockUnit.create(newStockUnit);
+        return stockUnit;
+    };
 
-export const getAllStockUnit = async (): Promise<IStockUnitDoc[]> => {
-    const StockUnit = DbConnectionManager.getTenantModel<IStockUnitDoc>(
-        MONGOOSE_MODELS.TENANT_DB.CATALOGUE.STOCKUNIT,
-    );
-    let allStockUnit = await StockUnit.find({});
-    if (isEmpty(allStockUnit)) allStockUnit = await insertDefaultStockUnit(StockUnit);
-    return allStockUnit;
-};
+    static searchStockUnit = async (query: string): Promise<IStockUnitDoc[]> => {
+        const StockUnit = StockUnitDbService.getModal();
+        const matchingStockUnits = await StockUnit.find({
+            $or: [{ name: new RegExp(`^${query}`, 'i') }, { unit: new RegExp(`^${query}`, 'i') }],
+        });
+        return matchingStockUnits;
+    };
 
-export const getStockUnit = async (brandId: string): Promise<IStockUnitDoc> => {
-    const StockUnit = DbConnectionManager.getTenantModel<IStockUnitDoc>(
-        MONGOOSE_MODELS.TENANT_DB.CATALOGUE.STOCKUNIT,
-    );
-    const stockUnit = await StockUnit.findById(brandId);
-    return stockUnit;
-};
+    static getAllStockUnit = async (): Promise<IStockUnitDoc[]> => {
+        const StockUnit = StockUnitDbService.getModal();
+        let allStockUnit = await StockUnit.find({});
+        if (isEmpty(allStockUnit))
+            allStockUnit = await StockUnitDbService.insertDefaultStockUnit(StockUnit);
+        return allStockUnit;
+    };
 
-export const editStockUnit = async (
-    stockUnitId: string,
-    updatedStockUnit: IEditStockUnitRequest,
-): Promise<IStockUnitDoc> => {
-    const StockUnit = DbConnectionManager.getTenantModel<IStockUnitDoc>(
-        MONGOOSE_MODELS.TENANT_DB.CATALOGUE.STOCKUNIT,
-    );
-    const stock = await StockUnit.findByIdAndUpdate(stockUnitId, updatedStockUnit, {
-        new: true,
-    });
-    return stock;
-};
+    static getStockUnit = async (brandId: string): Promise<IStockUnitDoc> => {
+        const StockUnit = StockUnitDbService.getModal();
+        const stockUnit = await StockUnit.findById(brandId);
+        return stockUnit;
+    };
 
-export const deleteStockUnit = async (stockUnit: string): Promise<void> => {
-    const StockUnit = DbConnectionManager.getTenantModel<IStockUnitDoc>(
-        MONGOOSE_MODELS.TENANT_DB.CATALOGUE.STOCKUNIT,
-    );
-    await StockUnit.deleteOne({ _id: stockUnit });
-};
+    static editStockUnit = async (
+        stockUnitId: string,
+        updatedStockUnit: IEditStockUnitRequest,
+    ): Promise<IStockUnitDoc> => {
+        const StockUnit = StockUnitDbService.getModal();
+        const stock = await StockUnit.findByIdAndUpdate(stockUnitId, updatedStockUnit, {
+            new: true,
+        });
+        return stock;
+    };
 
-const insertDefaultStockUnit = async (
-    StockUnit: Model<IStockUnitDoc>,
-): Promise<IStockUnitDoc[]> => {
-    const stockUnits = await StockUnit.insertMany(defaultStockUnits, { lean: true });
-    return stockUnits;
-};
+    static deleteStockUnit = async (stockUnit: string): Promise<void> => {
+        const StockUnit = StockUnitDbService.getModal();
+        await StockUnit.deleteOne({ _id: stockUnit });
+    };
+
+    private static insertDefaultStockUnit = async (
+        StockUnit: Model<IStockUnitDoc>,
+    ): Promise<IStockUnitDoc[]> => {
+        const stockUnits = await StockUnit.insertMany(defaultStockUnits, { lean: true });
+        return stockUnits;
+    };
+}
